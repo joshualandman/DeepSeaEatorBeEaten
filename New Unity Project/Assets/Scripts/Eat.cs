11de1478011575b7c3b4f4c20bb2846286871bd0
JoshuaLandman <jsl9284@rit.edu>
2014-11-10 08:21:26 -0500

    Eat got a high five
    Code was commented, tried to fix null error, can't fix with collision as
    it is

--- New Unity Project/Assets/Scripts/Eat.cs
+++ New Unity Project/Assets/Scripts/Eat.cs
@@ -3,26 +3,32 @@ using System.Collections;
 
 public class Eat : MonoBehaviour {
 
+	//declare player and enemy and their size numbers
 	GameObject player;
 	GameObject enemy;
-	float playerNum;
-	float enemyNum;
+	float playerNum = 0.0f;
+	float enemyNum = 0.0f;
 
 	void Start()
 	{
+		//set player equal to player and set his size
 		player = GameObject.Find("Player");
 		playerNum = player.GetComponent<Player>().bodySize;
 	}
 	
 	void OnCollisionEnter2D(Collision2D other)
-	{		
+	{
+		//set enemy equal to enemy and set his size
 		enemy = GameObject.Find(other.gameObject.name);
 		enemyNum = other.gameObject.GetComponent<EnemyMovement>().bodySize;
 
+		//loop through the transforms in other
 		foreach(Transform target in other.transform)
 		{
+			//find the child that we want
 			if(target.gameObject.name == "body")
 			{
+				//if enemy is smaller than player, get eaten
 				if(enemyNum < playerNum)
 				{
 					eatEnemy(target.gameObject);
@@ -34,11 +40,14 @@ public class Eat : MonoBehaviour {
 	
 	void eatEnemy(GameObject gameobject)
 	{
+		//save player scale
 		Vector3 newScale = transform.parent.localScale;
-		
+
+		//enlarge scale by rel num
 		newScale.x = newScale.x * 1.06f;
 		newScale.y = newScale.y * 1.06f;
-		
+
+		//change player's scale to new scale
 		transform.parent.localScale = newScale;
 	}
 }
