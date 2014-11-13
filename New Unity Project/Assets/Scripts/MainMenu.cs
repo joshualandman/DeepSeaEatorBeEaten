5847502d7cc8a1b01b3cfea18e6e399bf191829b
Seithr <slytherin.lion@verizon.net>
2014-11-13 13:33:41 -0500

    Menus
    Problem: i can't figure out how to get the buttons to always be the
    right size in the right place regardless of screen size. Using rederer
    data doesn't scale with the scene as far as I can tell, and I don't want
    to use scene because when i open the scene there are always margins of
    varying sizes that would also throw the buttons off

---
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	//control variables
+	bool startClick = false;
+	bool instructClick = false;
+	bool backClick = false;
+	bool main = true;
+
+	//button textures
+	public Texture2D startButton;
+	public Texture2D instructButton;
+	public Texture2D backButton;
+
+	//menue textures
+	public SpriteRenderer start;
+	public SpriteRenderer instruct;
+
+	//sets instructions as invisible to start
+	void Awake()
+	{
+		instruct.color = new Color(1f,1f,1f,0f);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//display main menu
+		if(main == true)
+		{
+			//start the game
+			if(startClick == true)
+			{
+				Application.LoadLevel("test");
+			}
+			//switch view to instruction screen
+			else if(instructClick == true)
+			{
+				start.color = new Color(1f,1f,1f,0f);
+				instruct.color = new Color(1f,1f,1f,1f);
+				main = false;
+				//makes sure the buttons read as unclicked
+				instructClick = false;
+				startClick = false;
+			}
+		}
+		//display instructions
+		else
+		{
+			if(backClick == true)
+			{
+				start.color = new Color(1f,1f,1f,1f);
+				instruct.color = new Color(1f,1f,1f,0f);
+				main = true;
+				//makes sure the button reads as unclicked
+				backClick = false;
+			}
+		}
+	}//ends:Update
+
+	void OnGUI ()
+	{
+		//Main menu GUI
+		if(main == true)
+		{
+			//if the player wants to start the game
+			if(GUI.Button(new Rect(259.5f,620f,445f,101.5f),startButton))
+			{
+				startClick = true;
+			}
+			//if the player wants to see the instructions
+			else if(GUI.Button(new Rect(700f,620f,445f,101.5f),instructButton))
+			{
+				instructClick = true;
+			}
+		}
+		//Instructions GUI
+		else
+		{
+			//if the player wants to leave the instructions menu
+			if(GUI.Button(new Rect(835f,765f,277f,112.5f),backButton))
+			{
+				backClick = true;
+			}
+		}
+	}//ends: OnGUI
+}
