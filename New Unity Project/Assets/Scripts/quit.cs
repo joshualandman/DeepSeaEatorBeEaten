0c36b8e3348a2ab2007cd2fb8791a6877aa3a779
Christopher Tudor <cdt8614@rit.edu>
2014-12-16 10:07:52 -0500

    Quit during run and executable files

---
+++ New Unity Project/Assets/Scripts/quit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quit : MonoBehaviour {
+
+	void Awake()
+	{
+		DontDestroyOnLoad (this);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+}
